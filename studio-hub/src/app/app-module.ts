47cf65970ab2b535ac823e746ecd963dca9b67b4
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-04-09 18:01:45 -0400

    navbar and footer creation

--- studio-hub/src/app/app-module.ts
+++ studio-hub/src/app/app-module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Home } from './features/home/home';
 import { Navbar } from './shared/components/navbar/navbar';
+import { Footer } from './shared/components/footer/footer';
 
 @NgModule({
-  declarations: [App, Home, Navbar],
+  declarations: [App, Home, Navbar, Footer],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
