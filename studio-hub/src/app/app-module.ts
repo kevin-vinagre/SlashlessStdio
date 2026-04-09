f243107d85426d8c0b90c6786fcc168a54fec926
kevin-vinagre <lkevinvinagre@hotmail.com>
2026-04-09 16:32:17 -0400

    home page and navbar component creation

--- studio-hub/src/app/app-module.ts
+++ studio-hub/src/app/app-module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Home } from './features/home/home';
+import { Navbar } from './shared/components/navbar/navbar';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Home, Navbar],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
